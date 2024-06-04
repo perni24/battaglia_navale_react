47e09a1dd6b07065590a23c56517043264eaf027
perni24 <perni24lb@gmail.com>
2024-06-04 18:05:35 +0200

    modifica 04/06/2024

--- src/components/multiPlayer/homeMultiPlayer.jsx
+++ src/components/multiPlayer/homeMultiPlayer.jsx
@@ -1,7 +1,49 @@
+import { useEffect } from 'react';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from '../../firebaseConfig';
+import { getDatabase, ref, onValue } from "firebase/database";
+
 function homeMultiPlayer() {
+
+    var roomData = {id: 1}
+
+    // Effettua la richiesta dei dati al database e visualizzali nella console
+    const requestAllData = () => {
+        // Inizializza l'app Firebase
+        const firebaseApp = initializeApp(firebaseConfig);
+
+        // Ottieni un riferimento al nodo nel database da cui desideri recuperare i dati
+        const databaseRef = ref(getDatabase(firebaseApp));
+
+        // Ascolta gli eventi "value" sul nodo del database e recupera i dati
+        onValue(databaseRef, (snapshot) => {
+            const data = snapshot.val();
+            console.log("Tutti i dati dal database:", data);
+        });
+    };
+
+    const handleGenerateRoomClick = () => {
+        const firebaseApp = initializeApp(firebaseConfig); // Inizializza l'app Firebase
+
+        const databaseRef = ref(getDatabase(firebaseApp)); // Ottieni un riferimento al nodo del database in cui desideri salvare i dati
+
+        // Salva i dati della stanza nel database
+        set(databaseRef, roomData)
+            .then(() => {
+                console.log("Dati della stanza salvati con successo nel database.");
+            })
+            .catch((error) => {
+                console.error("Errore durante il salvataggio dei dati della stanza nel database:", error);
+            });
+    };
+   /* // Chiamare la funzione per richiedere i dati quando il componente viene montato
+    useEffect(() => {
+        requestAllData();
+    }, []);*/
+
     return (
         <>
-            <button >genera stanza</button>
+            <button onClick={requestAllData}>genera stanza</button>
             <button>unisciti</button>
         </>
     );
---
+++ src/firebaseConfig.jsx
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBmXAsOrvJjTB4xFc4wlp6zmVaOAabGZmQ",
+  authDomain: "battaglia-navale-e7827.firebaseapp.com",
+  databaseURL: "https://battaglia-navale-e7827-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "battaglia-navale-e7827",
+  storageBucket: "battaglia-navale-e7827.appspot.com",
+  messagingSenderId: "659357691717",
+  appId: "1:659357691717:web:84412ca86c2804c51b982c",
+  measurementId: "G-BC426K9TQ3"
+}
